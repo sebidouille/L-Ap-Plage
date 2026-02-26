2bddba9b8177b58c561823cb83bb2cbbbba5e64c
sebidouille <53144584+sebidouille@users.noreply.github.com>
2026-02-26 11:03:40 +0100

    Update app.js

--- app.js
+++ app.js
@@ -66,8 +66,8 @@ function initMap() {
         attributionControl: false
     }).setView(CONFIG.GROIX_CENTER, CONFIG.ZOOM_LEVEL);
     
-    // Mapbox - Style personnalisé avec Default public token
-    L.tileLayer('https://api.mapbox.com/styles/v1/rouxseb/cmm20rkrd004w01s7clxx5kgk/tiles/{z}/{x}/{y}?access_token=pk.eyJ1Ijoicm91eHNlYiIsImEiOiJjbW0xd3dvcTAwMTZzMnJzZXdyYXFpMjBvIn0.Tq3uFh1jH5n-7OXcfm7MtQ', {
+    // Mapbox - Style personnalisé sans POI commerces
+    L.tileLayer('https://api.mapbox.com/styles/v1/rouxseb/cmm38wphm004d01s69zh359me/tiles/{z}/{x}/{y}?access_token=pk.eyJ1Ijoicm91eHNlYiIsImEiOiJjbW0xd3dvcTAwMTZzMnJzZXdyYXFpMjBvIn0.Tq3uFh1jH5n-7OXcfm7MtQ', {
         attribution: '© <a href="https://www.mapbox.com/">Mapbox</a>',
         tileSize: 512,
         zoomOffset: -1,
@@ -75,7 +75,7 @@ function initMap() {
         minZoom: 11
     }).addTo(map);
     
-    console.log('Carte Mapbox style personnalisé (default token)');
+    console.log('Carte Mapbox style personnalisé chargée');
     
     // Ajouter le bouton de géolocalisation
     addGeolocationButton();
